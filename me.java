7d3e814355a6e1f68dde44bbe096000179997e6f
Ammar Faizi <ammarfaizi2@gmail.com>
2017-06-19 14:21:47 +0700

    Latihan trim

--- me.java
+++ me.java
@@ -3,10 +3,10 @@ import p1.*;
 public class me{
 	public String hl = "Hello world";
 	public static void main(String[] args){
-		String nama = new String("Ammar Faizi");
+		String nama = new String("Ammar Faizi          ");
 		char kelas[] = {'X','M','I','P','A'};
 		String Kelas = new String(kelas);
-		String out = (nama+"\n"+Kelas+'5').toLowerCase();
+		String out = ((nama).trim()+"q\n"+Kelas+'5').toLowerCase();
 		System.out.print(out);
 	}
 	public String a(){
