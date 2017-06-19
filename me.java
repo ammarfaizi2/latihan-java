f752c109dbad1c7f6df04aa5b76bc92e7f1c7a6f
Ammar Faizi <ammarfaizi2@gmail.com>
2017-06-19 14:03:58 +0700

    Latihan membuat property

--- me.java
+++ me.java
@@ -3,7 +3,7 @@ import p1.*;
 public class me{
 	public static void main(String[] args){
 		a1 ax = new a1();
-		System.out.print(ax.steks()+"\n");
+		System.out.print(ax.steks()+"\n"+ax.alamat+"\n");
 		System.out.print(a1.nama);
 	}
 }
--- p1/a1.java
+++ p1/a1.java
@@ -2,6 +2,7 @@ package p1;
 
 public class a1{
 	public static String nama = "Ammar";
+	public String alamat = "Candi Asri 1 No.125 RT38 RW09";
 	public String steks(){
 		return "Ini adalah sebuah teks";
 	}
