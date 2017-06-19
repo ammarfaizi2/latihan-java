9a1f01746369b350d353fb00cfcb46d671f4f90a
Ammar Faizi <ammarfaizi2@gmail.com>
2017-06-19 13:57:34 +0700

    first commit

---
+++ me.java
@@ -0,0 +1,8 @@
+import p1.*;
+
+public class me{
+	public static void main(String[] args){
+		a1 ax = new a1();
+		System.out.print(ax.steks());
+	}
+}
---
+++ p1/a1.java
@@ -0,0 +1,7 @@
+package p1;
+
+public class a1{
+	public String steks(){
+		return "Ini adalah sebuah teks";
+	}
+}
