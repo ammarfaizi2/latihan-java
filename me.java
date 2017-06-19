65543bd0cf0a59e3132543c602863de995424548
Ammar Faizi <ammarfaizi2@gmail.com>
2017-06-19 14:24:51 +0700

    Latihan array

--- me.java
+++ me.java
@@ -3,11 +3,13 @@ import p1.*;
 public class me{
 	public String hl = "Hello world";
 	public static void main(String[] args){
-		String nama = new String("Ammar Faizi          ");
+		/*String nama = new String("Ammar Faizi          ");
 		char kelas[] = {'X','M','I','P','A'};
 		String Kelas = new String(kelas);
 		String out = ((nama).trim()+"q\n"+Kelas+'5').toLowerCase();
-		System.out.print(out);
+		System.out.print(out);*/
+		String[] nama = {"Ammar", "Kevin", "Akbar"};
+		System.out.print(nama[0]);
 	}
 	public String a(){
 		return "aaaaaaa";
