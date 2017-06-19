7dd5a9d7de63aee41d4c1d338d26c95ac9cabf5d
Ammar Faizi <ammarfaizi2@gmail.com>
2017-06-19 14:02:58 +0700

    Latihan membuat property static

--- me.java
+++ me.java
@@ -3,6 +3,7 @@ import p1.*;
 public class me{
 	public static void main(String[] args){
 		a1 ax = new a1();
-		System.out.print(ax.steks());
+		System.out.print(ax.steks()+"\n");
+		System.out.print(a1.nama);
 	}
 }
--- p1/a1.java
+++ p1/a1.java
@@ -1,6 +1,7 @@
 package p1;
 
 public class a1{
+	public static String nama = "Ammar";
 	public String steks(){
 		return "Ini adalah sebuah teks";
 	}
