7def9eb5bfa56de7131802074b1e9b90442243ba
Ammar Faizi <ammarfaizi2@gmail.com>
2017-06-19 14:07:38 +0700

    Latihan OOP

--- me.java
+++ me.java
@@ -3,7 +3,9 @@ import p1.*;
 public class me{
 	public static void main(String[] args){
 		a1 ax = new a1();
-		System.out.print(ax.steks()+"\n"+ax.alamat+"\n");
-		System.out.print(a1.nama);
+		System.out.print(ax.steks()+"\n"+ax.alamat+"\n"+ax.nama+"\n"+(new me()).a());
+	}
+	public String a(){
+		return "aaaaaaa";
 	}
 }
